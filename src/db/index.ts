47ce81e677c4c9e9e03a8ea9aad0398d7d6b52cf
Md Nazmul Bijoy <nazmulbijoy9105@gmail.com>
2026-03-20 21:05:45 +0600

    fix: remove kilo db folder

--- src/db/index.ts
+++
@@ -1,9 +0,0 @@
-import { createDatabase } from "@kilocode/app-builder-db";
-import * as schema from "./schema";
-
-const dbUrl = process.env.DB_URL;
-const dbToken = process.env.DB_TOKEN;
-
-export const db = dbUrl && dbToken ? createDatabase(schema) : null;
-
-export const isDbConnected = () => db !== null;
--- src/db/migrations/0001_greedy_master_mold.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `users` ADD `image` text;--> statement-breakpoint
-ALTER TABLE `users` ADD `email_verified` integer;
